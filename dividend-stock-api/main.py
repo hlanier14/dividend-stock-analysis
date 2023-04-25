88dc22c9e8f550b0d17a1d96f05bd0610e9b2ab0
Harrison Lanier <harrison.lanier@clickvoyant.com>
2023-04-24 21:18:02 -0500

    fill nan in api response

--- dividend-stock-api/main.py
+++ dividend-stock-api/main.py
@@ -1,5 +1,5 @@
 from google.cloud import bigquery
-from flask import jsonify
+import json
 import pandas as pd
 from dotenv import load_dotenv
 
@@ -33,36 +33,43 @@ def main(request):
     dividend_metadata_job = bg_client.query(f"SELECT * FROM `{DIVIDEND_META_TABLE_ID}` WHERE consecutiveYears >= 5;")
     dividend_metadata_job.result()
     dividend_metadata = dividend_metadata_job.to_dataframe()
+    dividend_metadata.fillna(-1, inplace=True)
 
     tickers = dividend_metadata['ticker'].to_list()
 
     company_data_job = bg_client.query(f"SELECT * FROM `{COMPANY_TABLE_ID}` WHERE ticker IN UNNEST({tickers});")
     company_data_job.result()
     company_data = company_data_job.to_dataframe()
+    company_data.fillna(-1, inplace=True)
 
     financials_data_job = bg_client.query(f"SELECT * FROM `{FINANCIALS_TABLE_ID}` WHERE ticker IN UNNEST({tickers});")
     financials_data_job.result()
     financials_data = financials_data_job.to_dataframe()
     financials_data['exDividendDate'] = pd.to_datetime(financials_data['exDividendDate'])
     financials_data['exDividendDate'] = financials_data['exDividendDate'].dt.strftime('%Y-%m-%d')
+    financials_data.fillna(-1, inplace=True)
 
     dividend_data_job = bg_client.query(f"SELECT * FROM `{DIVIDEND_TABLE_ID}` WHERE ticker IN UNNEST({tickers}) AND date >= DATE_SUB(CURRENT_DATE, INTERVAL 1 YEAR);")
     dividend_data_job.result()
     dividend_data = dividend_data_job.to_dataframe()
     dividend_data['date'] = pd.to_datetime(dividend_data['date'])
+    dividend_data.fillna(-1, inplace=True)
 
     price_data_job = bg_client.query(f"SELECT * FROM `{PRICE_TABLE_ID}` WHERE ticker IN UNNEST({tickers}) AND date >= DATE_SUB(CURRENT_DATE, INTERVAL 30 DAY);")
     price_data_job.result()
     price_data = price_data_job.to_dataframe()
     price_data['date'] = pd.to_datetime(price_data['date'])
+    price_data.fillna(-1, inplace=True)
 
     price_metadata_job = bg_client.query(f"SELECT * FROM `{PRICE_METADATA_TABLE_ID}` WHERE ticker IN UNNEST({tickers});")
     price_metadata_job.result()
     price_metadata = price_metadata_job.to_dataframe()
+    price_metadata.fillna(-1, inplace=True)
 
     benchmark_metadata_job = bg_client.query(f"SELECT * FROM `{BENCHMARK_METADATA_TABLE_ID}`;")
     benchmark_metadata_job.result()
     benchmark_metadata = benchmark_metadata_job.to_dataframe()
+    benchmark_metadata.fillna(-1, inplace=True)
 
     data = []
     for ticker in tickers:
@@ -102,6 +109,7 @@ def main(request):
         }
 
         valuation = (dividends['last'] * dividends['metadata']['dividendFrequency']) / (required_rate['value'] - dividends['metadata']['fiveYearCAGR'])
+
         ddm = {
             'value': valuation,
             'pctChange': (valuation - price['last']) / price['last'],
@@ -112,13 +120,8 @@ def main(request):
             }
         }
 
-        try:
-            ticker_financials = financials_data.loc[financials_data['ticker'] == ticker, ~financials_data.columns.isin(['ticker'])].to_dict('records')[0]
-        except:
-            continue 
-
         ticker_data = company_data.loc[company_data['ticker'] == ticker].to_dict('records')[0]
-        ticker_data['financials'] = ticker_financials
+        ticker_data['financials'] = financials_data.loc[financials_data['ticker'] == ticker, ~financials_data.columns.isin(['ticker'])].to_dict('records')[0]
         ticker_data['dividends'] = dividends
         ticker_data['price'] = price
         ticker_data['requiredRate'] = required_rate
@@ -126,11 +129,13 @@ def main(request):
 
         data.append(ticker_data)
 
-    response = jsonify(data)
+    response = json.dumps(data)
     
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    response.headers.add('Access-Control-Allow-Methods', 'GET')
-    response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
+    # response.headers.add('Access-Control-Allow-Origin', '*')
+    # response.headers.add('Access-Control-Allow-Methods', 'GET')
+    # response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
 
     return response, 200
 
+if __name__ == "__main__":
+    main({})
