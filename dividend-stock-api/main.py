79db2fc74f18b9e3d2081edbd2e73cf927f94e9e
Harrison Lanier <harrison.lanier@clickvoyant.com>
2023-04-24 20:44:43 -0500

    added benchmark rates and beta to api

--- dividend-stock-api/main.py
+++ dividend-stock-api/main.py
@@ -14,12 +14,16 @@ DIVIDEND_TABLE = 'dividends'
 DIVIDEND_META_TABLE = 'dividend_metadata'
 COMPANY_TABLE = 'companies'
 FINANCIALS_TABLE = 'company_financials'
+PRICE_METADATA_TABLE = 'price_metadata'
+BENCHMARK_METADATA_TABLE = 'benchmark_metadata'
 
 PRICE_TABLE_ID = f'{PROJECT_ID}.{DATASET_ID}.{PRICE_TABLE}'
+PRICE_METADATA_TABLE_ID = f'{PROJECT_ID}.{DATASET_ID}.{PRICE_METADATA_TABLE}'
 DIVIDEND_TABLE_ID = f'{PROJECT_ID}.{DATASET_ID}.{DIVIDEND_TABLE}'
 DIVIDEND_META_TABLE_ID = f'{PROJECT_ID}.{DATASET_ID}.{DIVIDEND_META_TABLE}'
 COMPANY_TABLE_ID = f'{PROJECT_ID}.{DATASET_ID}.{COMPANY_TABLE}'
 FINANCIALS_TABLE_ID = f'{PROJECT_ID}.{DATASET_ID}.{FINANCIALS_TABLE}'
+BENCHMARK_METADATA_TABLE_ID = f'{PROJECT_ID}.{DATASET_ID}.{BENCHMARK_METADATA_TABLE}'
 
 
 def main(request):
@@ -52,6 +56,14 @@ def main(request):
     price_data = price_data_job.to_dataframe()
     price_data['date'] = pd.to_datetime(price_data['date'])
 
+    price_metadata_job = bg_client.query(f"SELECT * FROM `{PRICE_METADATA_TABLE_ID}` WHERE ticker IN UNNEST({tickers});")
+    price_metadata_job.result()
+    price_metadata = price_metadata_job.to_dataframe()
+
+    benchmark_metadata_job = bg_client.query(f"SELECT * FROM `{BENCHMARK_METADATA_TABLE_ID}`;")
+    benchmark_metadata_job.result()
+    benchmark_metadata = benchmark_metadata_job.to_dataframe()
+
     data = []
     for ticker in tickers:
 
@@ -72,8 +84,32 @@ def main(request):
 
         dividends = {
             'last': last_dividend,
-            'metadata': dividend_metadata.loc[dividend_metadata['ticker'] == ticker, ~dividend_metadata.columns.isin(['ticker'])].to_dict('records'),
+            'metadata': dividend_metadata.loc[dividend_metadata['ticker'] == ticker, ~dividend_metadata.columns.isin(['ticker'])].to_dict('records')[0],
             'historical': ticker_dividends[['date', 'dividend']].to_dict('records')
+        }
+
+        beta = price_metadata.loc[price_metadata['ticker'] == ticker, ~price_metadata.columns.isin(['ticker'])].to_dict('records')[0]['fiveYearBeta']
+        risk_free_rate = benchmark_metadata.loc[benchmark_metadata['name'] == 'Latest 10 YR T-Bill Rate', ~benchmark_metadata.columns.isin(['name'])].to_dict('records')[0]['value']
+        exp_market_rate = benchmark_metadata.loc[benchmark_metadata['name'] == 'S&P 500 5 Year CAGR', ~benchmark_metadata.columns.isin(['name'])].to_dict('records')[0]['value']
+
+        required_rate = {
+            'value': risk_free_rate + beta * (exp_market_rate - risk_free_rate),
+            'components': {
+                'riskFreeRate': risk_free_rate,
+                'expectedMarketRate': exp_market_rate,
+                'beta': beta
+            }
+        }
+
+        valuation = (dividends['last'] * dividends['metadata']['dividendFrequency']) / (required_rate['value'] - dividends['metadata']['fiveYearCAGR'])
+        ddm = {
+            'value': valuation,
+            'pctChange': (valuation - price['last']) / price['last'],
+            'components': {
+                'forwardDividends': dividends['last'] * dividends['metadata']['dividendFrequency'],
+                'requiredRate': required_rate['value'],
+                'dividendGrowth': dividends['metadata']['fiveYearCAGR']
+            }
         }
 
         try:
@@ -85,6 +121,8 @@ def main(request):
         ticker_data['financials'] = ticker_financials
         ticker_data['dividends'] = dividends
         ticker_data['price'] = price
+        ticker_data['requiredRate'] = required_rate
+        ticker_data['ddm'] = ddm
 
         data.append(ticker_data)
 
